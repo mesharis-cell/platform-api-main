300487a35f64406bbd31092f49ff996a2374a78d
riyazulhaque64 <riyazulhaque64@gmail.com>
2025-12-24 18:50:17 +0600

    update

--- generate-swagger.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-import swaggerSpec from "./src/swagger";
-
-const outputPath = path.join(__dirname, "src/swagger-output.json");
-
-fs.writeFileSync(outputPath, JSON.stringify(swaggerSpec, null, 2));
-
-console.log(`Swagger JSON generated at ${outputPath}`);
