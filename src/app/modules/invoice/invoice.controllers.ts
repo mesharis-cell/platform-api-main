ceed6078054253f17a58b5dd2f0712c917a66505
riyazulhaque64 <riyazulhaque64@gmail.com>
2026-01-08 23:54:09 +0600

    implment cost estimate pdf

--- src/app/modules/invoice/invoice.controllers.ts
+++ src/app/modules/invoice/invoice.controllers.ts
@@ -160,8 +160,10 @@ const downloadCostEstimatePDF = catchAsync(async (req, res) => {
 
     const s3Key = `cost-estimates/${order.company.name.replace(/\s/g, '-').toLowerCase()}/${order.order_id}.pdf`
 
+    console.log(s3Key);
+
     const buffer = await getPDFBufferFromS3(s3Key);
-    const fileName = `${order.order_id}.pdf`;
+    const fileName = `cost-estimate-${order.order_id}.pdf`;
 
     // Set headers for PDF download
     res.setHeader('Content-Type', 'application/pdf');
