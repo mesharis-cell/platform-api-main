e8c74a5fcfc64d4c9f8ebc6ec05165d10fa49792
codemine <codemine24@gmail.com>
2026-02-07 18:37:41 +0600

    feature validator middleware updated

--- src/app/middleware/feature-validator.ts
+++ src/app/middleware/feature-validator.ts
@@ -3,7 +3,7 @@ import httpStatus from "http-status"
 import CustomizedError from "../error/customized-error"
 import { and, eq } from "drizzle-orm"
 import { db } from "../../db"
-import { companies } from "../../db/schema"
+import { companies, platforms } from "../../db/schema"
 
 const featureValidator = (featureName: string) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -22,10 +22,29 @@ const featureValidator = (featureName: string) => {
       }
 
       const companyFeatures = company[0].features as Record<string, boolean>;
-      const isFeatureEnabled = companyFeatures?.[featureName] === true;
 
-      if (!isFeatureEnabled) {
-        throw new CustomizedError(httpStatus.FORBIDDEN, `Feature ${featureName.replace(/_/g, " ")} is not enabled for this company`);
+      // check if feature is enabled for company if feature name is present in company features
+      if (companyFeatures.hasOwnProperty(featureName)) {
+        const isFeatureEnabled = companyFeatures?.[featureName] === true;
+
+        if (!isFeatureEnabled) {
+          throw new CustomizedError(httpStatus.FORBIDDEN, `Feature ${featureName.replace(/_/g, " ")} is not enabled for this company`);
+        }
+      } else {
+        // check if feature is enabled for platform if feature name is not present in company features
+        const platform = await db.select({ features: platforms.features })
+          .from(platforms)
+          .where(eq(platforms.id, platformId))
+          .limit(1)
+          .execute();
+
+        const platformFeatures = platform[0].features as Record<string, boolean>;
+        const isPlatformFeatureEnabled = platformFeatures?.[featureName] === true;
+
+        // if feature is not enabled for platform then throw error
+        if (!isPlatformFeatureEnabled) {
+          throw new CustomizedError(httpStatus.FORBIDDEN, `Feature ${featureName.replace(/_/g, " ")} is not enabled for this company`);
+        }
       }
       next();
     } catch (error) {
