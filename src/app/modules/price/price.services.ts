fc92810cc73e50d4c04f2f280b3033cfe65aaea3
riyazulhaque64 <riyazulhaque64@gmail.com>
2026-02-07 18:38:03 +0600

    update

--- src/app/modules/price/price.services.ts
+++ src/app/modules/price/price.services.ts
@@ -1,10 +1,11 @@
 import { and, eq } from "drizzle-orm";
 import { db } from "../../../db";
-import { prices } from "../../../db/schema";
+import { orders, prices } from "../../../db/schema";
 import httpStatus from "http-status";
 import CustomizedError from "../../error/customized-error";
 import { AuthUser } from "../../interface/common";
 import { UpdatePriceForTransportPayload } from "./price.interfaces";
+import { costEstimateGenerator } from "../../utils/cost-estimate";
 
 const updatePriceForTransport = async (
     id: string,
@@ -22,6 +23,12 @@ const updatePriceForTransport = async (
 
     if (!priceRecord) {
         throw new CustomizedError(httpStatus.NOT_FOUND, "Pricing details not found");
+    }
+
+    const [order] = await db.select().from(orders).where(eq(orders.order_pricing_id, priceRecord.id)).limit(1);
+
+    if (!order) {
+        throw new CustomizedError(httpStatus.NOT_FOUND, "Order not found for this pricing");
     }
 
     // Step 2: Recalculate new pricing details
@@ -49,6 +56,22 @@ const updatePriceForTransport = async (
 
     // Step 3: Update price details
     const [updatedPrice] = await db.update(prices).set(pricingDetails).where(eq(prices.id, id)).returning();
+
+    if ([
+        "QUOTED",
+        "DECLINED",
+        "CONFIRMED",
+        "AWAITING_FABRICATION",
+        "IN_PREPARATION",
+        "READY_FOR_DELIVERY",
+        "IN_TRANSIT",
+        "DELIVERED",
+        "IN_USE",
+        "AWAITING_RETURN",
+        "RETURN_IN_TRANSIT"
+    ].includes(order.order_status)) {
+        await costEstimateGenerator(order.id, platformId, user, true);
+    }
 
     return updatedPrice;
 };
