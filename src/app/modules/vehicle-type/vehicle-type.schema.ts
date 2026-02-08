5df78069d1eb8507721ddffc814c79251a875ff9
codemine24 <codemine24@gmail.com>
2026-02-08 23:03:11 +0600

    vehiclel update done

--- src/app/modules/vehicle-type/vehicle-type.schema.ts
+++ src/app/modules/vehicle-type/vehicle-type.schema.ts
@@ -21,7 +21,7 @@ const updateVehicleType = z.object({
     is_active: z.boolean().optional().default(true),
     display_order: z.number().optional().default(1),
     description: z.string().optional(),
-  }),
+  }).partial(),
 });
 
 export const vehicleTypeSchemas = {
