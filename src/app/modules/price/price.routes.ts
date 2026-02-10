59e30cc8cd5d23b19a8831ae0c24fbff622b4701
codemine <codemine24@gmail.com>
2026-02-10 23:24:09 +0600

    platform validator added in price route

--- src/app/modules/price/price.routes.ts
+++ src/app/modules/price/price.routes.ts
@@ -3,11 +3,13 @@ import { PriceControllers } from "./price.controllers";
 import auth from "../../middleware/auth";
 import payloadValidator from "../../middleware/payload-validator";
 import { PriceSchemas } from "./price.schemas";
+import platformValidator from "../../middleware/platform-validator";
 
 const router = express.Router();
 
 router.patch(
     "/transport/:id",
+    platformValidator,
     auth("ADMIN", "LOGISTICS"),
     payloadValidator(PriceSchemas.updatePriceForTransportSchema),
     PriceControllers.updatePriceForTransport
